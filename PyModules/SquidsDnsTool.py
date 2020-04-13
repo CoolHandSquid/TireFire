c7356bc9d5dbe6c29100868bc8e03997bb2364cf
CoolHandSquid <42553774+CoolHandSquid@users.noreply.github.com>
2020-04-13 16:39:19 -0400

    Add files via upload

--- PyModules/SquidsDnsTool.py
+++ PyModules/SquidsDnsTool.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os
 import sys
-	
+from colorama import Fore, Style
 
 try:
 	ip = sys.argv[1]
@@ -24,7 +24,7 @@ command2 = "dnsrecon -r 127.0.1.0/24 -n {}".format(ip)
 command3 = "dnsrecon -r {} -n {}".format(network, ip)
 command4 = "dig axfr @{}".format(ip)
 command5 = "dig axfr {} @{}".format(dn, ip)
-print("""Split the Term and Type these puppies in.
+print(Fore.GREEN + """Split the Term and Type these puppies in.
 ##############
 nslookup
 	SERVER {}
@@ -33,15 +33,23 @@ nslookup
 	{}
 	exit
 ##############""".format(ip, ip, dn))
-input(command1)
+#		input(Fore.GREEN + command )
+#		print(Style.RESET_ALL)
+
+input(Fore.GREEN + command1)
+print(Style.RESET_ALL)
 os.system(command1)
-input(command2)
+input(Fore.GREEN + command2)
+print(Style.RESET_ALL)
 os.system(command2)
-input(command3)
+input(Fore.GREEN + command3)
+print(Style.RESET_ALL)
 os.system(command3)
-input(command4)
+input(Fore.GREEN + command4)
+print(Style.RESET_ALL)
 os.system(command4)
-input(command5)
+input(Fore.GREEN + command5)
+print(Style.RESET_ALL)
 os.system(command5)
 
 print("Useage examle: python3 SquidsStupidDnsTool.py 192.168.11.1 yeet.wtf")
