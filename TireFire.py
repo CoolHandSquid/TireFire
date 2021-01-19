b53ab67cbe3f29cb8a3f0fc2a77b8101cc5ac020
CoolHandSquid <42553774+CoolHandSquid@users.noreply.github.com>
2021-01-19 11:40:09 -0500

    TireFire.py added
    TireFire.py added and partially tested

--- TireFire.py
+++ TireFire.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import os
 import sys
+import subprocess
 
 if len(sys.argv) != 2:
     print("The syntax I am looking for is more like TireFire 10.10.10.5")
     exit()
-ip  = sys.argv[1]
-os.system("tilix -t 'TireFire Main Page' -x $SHELL -c '/Yeet/Random/Udemy/TireFire/app.py {} ; $SHELL'".format(ip))
+ip      = sys.argv[1]
+path    = subprocess.getoutput("readlink /usr/bin/TireFire")
+path    = path[:-12]
+os.system("tilix -t 'TireFire Main Page' -x $SHELL -c '{}/app.py {} ; $SHELL'".format(path, ip))
