f57f3502f34166dff37965217d213899cd0d7d2c
CoolHandSquid <42553774+CoolHandSquid@users.noreply.github.com>
2021-01-19 11:38:53 -0500

    build.sh added
    build.sh added and partially tested

--- build.sh
+++ build.sh
@@ -1,1 +1,27 @@
-alias TireFire=$PWD/TireFire.py
+#!/usr/bin/env bash
+: <<'END'
+TireFire is installing...
+END
+
+git clone https://github.com/maurosoria/dirsearch.git
+chmod -R 755 dirsearch/
+
+rm /usr/bin/TireFire 2> /dev/null
+ln -s "$PWD/TireFire.py" "/usr/bin/TireFire"
+
+apt-get install tilix gobuster seclists -y
+
+wordlists=("/usr/share/wordlists/rockyou.txt" "/usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt" "/usr/share/dirbuster/wordlists/directory-list-2.3-small.txt" "/usr/share/seclists/Usernames/Names/names.txt")
+for wordlist in "${wordlists[@]}"
+do
+    if test ! -f "$wordlists"
+    then
+        echo "#"
+        echo "#"
+        echo "#"
+        echo "You are going to want to install $wordlist"
+        echo "#"
+        echo "#"
+        echo "#"
+    fi
+done
