e9506bc003355b5f07b9b92b428a5eb4b564456e
CoolHandSquid <42553774+CoolHandSquid@users.noreply.github.com>
2021-02-15 15:36:12 -0500

    Added Variables

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import re
 import sys
 import click
 import subprocess
+from netaddr    import *
 from colorama   import Fore, Style
 from tabulate   import tabulate
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -49,6 +50,18 @@ def start(IP):
         command = "nmap -Pn {} && nmap -sC -sV -Pn {} && nmap -p- -Pn {} && nmap -Pn -p- -sU {}".format(IP, IP, IP, IP)
         doit("Nmap", "Kickoff", command)
     return
+
+def get_network(IP):
+    try: 
+        IP      = IP +"/24"
+        IP      = IPNetwork(IP)
+        Network = IP.network
+        CIDR    = IP.prefixlen
+    except:
+        Network = "10.10.10.0"
+        CIDR    = "/24"
+    return Network, CIDR
+
 ##################################################
 #Begin Display
 ##################################################
@@ -76,13 +89,6 @@ def display_sub(proto):
                 scan    = eval("items[{}][0]".format(rawin))
                 command = eval("fullcmd[{}][0]".format(rawin)) 
                 if command[0] == '#':
-                    if command[1] == '#':
-                        pass
-                    else:
-                        try:
-                            command = (eval("f'" + command + "'"))
-                        except:
-                            pass
                     showit(proto, scan, command)    
                 elif "&&&&" in command:
                     command     = (eval("f'" + command + "'"))
@@ -97,14 +103,13 @@ def display_sub(proto):
                 display_main()
 
 def display_variables():
-    items   = [("IP", IP), ("Domain_Name", Domain_Name), ("Naming_Context", Naming_Context), ("Web_Proto", Web_Proto), ("Web_Port", Web_Port), ("Big_Passwordlist", Big_Passwordlist), ("Small_Passwordlist", Small_Passwordlist), ("Big_Userlist", Big_Userlist), ("Small_Userlist", Small_Userlist), ("Big_Dirlist", Big_Dirlist), ("Small_Dirlist", Small_Dirlist)]
+    items   = [("IP", IP), ("Network", Network), ("CIDR", CIDR), ("Domain_Name", Domain_Name), ("Naming_Context", Naming_Context), ("Web_Proto", Web_Proto), ("Web_Port", Web_Port), ("Username", Username), ("Password", Password), ("Big_Passwordlist", Big_Passwordlist), ("Small_Passwordlist", Small_Passwordlist), ("Big_Userlist", Big_Userlist), ("Small_Userlist", Small_Userlist), ("Big_Dirlist", Big_Dirlist), ("Small_Dirlist", Small_Dirlist)]
     headers = ["Variable", "Current Value"]
     rawin   = input(Fore.YELLOW + "Variables Table\n" + Style.RESET_ALL + tabulate(items, headers=headers, tablefmt="psql", showindex="always")+ "\n> ")
     if input_validation(items, rawin) == True:
         try:
             var = eval("items[{}][0]".format(rawin))
             globals()[var] = input("What would you like to set the {} to?\n> ".format(var))
-            #globals()[var] = varset(var)
             print(Fore.GREEN + "{} has been set to {}".format(var, globals()[var]) + Style.RESET_ALL)
         except:
             print("That did not seem to work. There was no variable change.")
@@ -127,7 +132,16 @@ def showit(proto, scan, command):
     #Called to write output in new tilix tab
     tab_name    = "{} {}".format(proto, scan)
     sfile = open('showit.txt', 'w')
-    sfile.write("\n{}\n".format(command))
+    scommand = command.split('\n')
+    if command[1] == "#":
+        sfile.write("\n{}\n".format(command))
+    else:
+        for line in scommand:
+            try:
+                line = (eval("f'" + line + "'"))
+            except:
+                pass
+            sfile.write("{}\n".format(line))
     sfile.close()
     command = "cat showit.txt"
     os.system("tilix -t '{}' -x $SHELL -c 'echo \"{}\"; {}; $SHELL'".format(tab_name, tab_name, command))
@@ -153,15 +167,15 @@ def input_validation(items, rawin):
             print(bad_input)
         return False
 
-def varset(var):
-    val = input("What would you like to set the {} to?\n> ".format(var))
-    return val
-
 IP              = sys.argv[1]
+Network         = get_network(IP)[0]
+CIDR            = get_network(IP)[1]
 Domain_Name     = "yee.wtf"
 Naming_Context  = "DC=YeetCannon,DC=local"
 Web_Proto       = "http"
 Web_Port        = "80"
+Username        = "Squid"
+Password        = "Y337C4nn0n!"
 Big_Passwordlist    = "/usr/share/wordlists/rockyou.txt"
 Small_Passwordlist  = "/usr/share/seclists/Passwords/darkweb2017-top1000.txt"
 Big_Userlist        = "/usr/share/seclists/Usernames/Names/names.txt"
