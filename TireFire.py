492b2598e3ac9f60f41ccafa7a7b4dfd0e43004e
CoolHandSquid <chrischmid32@gmail.com>
2021-10-20 15:33:01 -0400

    Post Port Updates

--- TireFire.py
+++ TireFire.py
@@ -7,14 +7,20 @@ import shlex
 import subprocess
 from netaddr import *
 
-parser = argparse.ArgumentParser(description='TireFire is Powered by book.hacktricks.xyz.')
+parser = argparse.ArgumentParser(
+        description='TireFire is Powered by book.hacktricks.xyz.',
+        epilog="TireFire -i tilix --update 10.169.254.56",)
 parser.add_argument("IP", help="IP adress or hostname of the target", type=str)
-parser.add_argument('-i', '--interface', default='tmux', help='Interact with TireFire via "tmux" or "tilix". tmux is default')
+parser.add_argument('-i', '--interaction_terminal', default='tmux', help='Interact with TireFire via "tmux" or "tilix". tmux is default')
 parser.add_argument('-u', '--updatedb', action="store_true", help='Update to the latest TireFire database') 
-args    = parser.parse_args()
+try:
+    args    = parser.parse_args()
+except:
+    print("example_syntax: {}".format(parser.epilog))
+    exit()
 
 cwd     = os.getcwd()
-tfdir   = subprocess.getoutput("readlink /usr/bin/TireFire")
+tfdir   = subprocess.getoutput("readlink /usr/local/bin/TireFire")
 tfdir   = tfdir[:-11]
 
 try:
@@ -25,7 +31,7 @@ except:
 
 def init_updatedb():
     print("Updating database")
-    subprocess.run(shlex.split("curl https://raw.githubusercontent.com/CoolHandSquid/TireFire/main/Main.csv --output {}/Main.csv".format(tfdir)), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(shlex.split("curl https://raw.githubusercontent.com/CoolHandSquid/TireFire/TireFire_V4/Main.csv --output {}/Main.csv".format(tfdir)), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print("Updating database complete")
 
 def init_TireFire_tmux():
@@ -40,6 +46,7 @@ def init_TireFire_tmux():
     subprocess.run(shlex.split("tmux send-keys -t TireFire_{}:0.0 '{}TireFire_tmux.py {} {} {} {}' Enter".format(hostname, tfdir, IP, hostname, cwd, tfdir)))
     print("TireFire session for {} named TireFire_{} has started successfully!\nList of running Tmux sessions:".format(IP, hostname))
     subprocess.run(shlex.split("tmux ls"))
+    print("\ncopy-paste (or hotkey w if already inside tmux):\ntmux a -t TireFire_{}".format(hostname))
 
 def init_TireFire_tilix():
     if pwd.getpwuid(os.getuid())[0] != "root":
@@ -54,12 +61,11 @@ def init_TireFire_tilix():
 if __name__ == "__main__":
     if args.updatedb == True:
         init_updatedb()
-    if args.interface == "tmux":
+    if args.interaction_terminal == "tmux":
         init_TireFire_tmux()
-    elif args.interface == "tilix":
+    elif args.interaction_terminal == "tilix":
         init_TireFire_tilix()
     else:
-        print("That interface is not valid. Please chose tmux or tilix(ex: --interface tilix).")
+        print("That interaction_terminal is not valid. Please chose tmux or tilix(ex: --interaction_terminal tilix).")
         exit()
-
 
