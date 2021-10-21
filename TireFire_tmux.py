887ff68ed83cc2fc0bc173eb64818b6e85a1b880
Your Name <you@example.com>
2021-10-21 07:45:19 -0400

    test

--- TireFire_tmux.py
+++ TireFire_tmux.py
@@ -87,6 +87,9 @@ def display_horizontal(items):
             nitems.append(aitems[line])
     return tabulate(nitems,headers=[" ", "Name", "Port", "Description", " ", "Name", "Port", "Description"], tablefmt="psql")
 
+def display_vertical(items):
+    pass
+
 def display_sub(proto):
     items   = db.get_display_sub(proto) 
     fullcmd = db.get_fullcommand(proto)
