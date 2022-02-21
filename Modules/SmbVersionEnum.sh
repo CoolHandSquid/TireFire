826ee81b09b9706cd17ba93ede1a4ea9885847a5
CoolHandSquid <42553774+CoolHandSquid@users.noreply.github.com>
2022-02-21 09:54:46 -0500

    Update SmbVersionEnum.sh
    MSF Support Added

--- Modules/SmbVersionEnum.sh
+++ Modules/SmbVersionEnum.sh
@@ -12,5 +12,8 @@ if [ ! -z $2 ]; then rport=$2; else rport=139; fi
 #tcpdump -s0 -n -i tun0 src $rhost and port $rport -A -c 10 2>/dev/null | grep -i "samba\|s.a.m" | tr -d '.' | grep -oP 'UnixSamba.*[0-9a-z]' | tr -d '\n' & echo -n "$rhost: " &
 tcpdump -s0 -n -i tun0 src $rhost and port $rport -A -c 10 &
 #echo "exit" | smbclient -L $rhost 1>/dev/null 2>/dev/null
-echo "exit" | smbclient -L \\10.11.1.115 --option='client min protocol'=LANMAN1
+echo "exit" | smbclient -L \\$1 --option='client min protocol'=LANMAN1
+if  type msfconsole >/dev/null; then 
+    msfconsole -q -x "use auxiliary/scanner/smb/smb_version; set rhost $1; run; exit"
+fi
 echo "" && sleep .1
